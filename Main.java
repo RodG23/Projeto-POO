e6cf6721ebbac744a95d5b9bea62091ae89c2805
Joao Magalhaes <jonasfmagalhaes29@gmail.com>
2023-04-20 18:04:49 +0100

    Restruturação da forma como os produtos ficam à venda.

--- Main.java
+++ Main.java
@@ -9,21 +9,18 @@ public class Main {
         Utilizador u1 = new Utilizador();
         Utilizador u2 = new Utilizador();
 
-        //perfil do utilizador u1 tem um mala e umas sapatilhas
-        u1.compra_Artigo(vinted, u1, a1, 2);
-        u1.compra_Artigo(vinted, u1, a2, 2);
-
-        //perfil do utilizador u2 tem um tshirt
-        u2.compra_Artigo(vinted, u2, a3, 2);
-
-        //utilizador u1 põe o artigo a1 à venda
+        //perfil do utilizador u1 tem um mala e umas sapatilhas à venda
         u1.aVenda_Artigo(vinted, a1);
+        u1.aVenda_Artigo(vinted, a2);
 
-        //artigo a1 fica em stock na vinted
+        //perfil do utilizador u2 tem um tshirt à venda
+        u2.aVenda_Artigo(vinted, a3);
+
+        //artigo a1, a2 e a3 estão em stock na vinted
         System.out.println(vinted.toString());
 
         //utilizador u2 compra artigo a1 ao utilizador u1
-        u2.compra_Artigo(vinted, u1, a1, 1);
+        u2.compra_Artigo(vinted, u1, a1);
 
         System.out.println(u2.toString());
         System.out.println(u1.toString());

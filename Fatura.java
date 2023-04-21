b26b74640d2d5ef57c11da12b27c5630e442fa01
RodG23 <rodrigofegomes2003@gmail.com>
2023-04-21 19:01:02 +0100

    Fatura

---
+++ Fatura.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+
+public class Fatura {
+    
+    private static int numFaturas = 0;
+
+    enum Tp {
+        COMPRA,
+        VENDA
+    }
+
+    private final int numEmissao;
+    private final Tp tipo;
+    private Set<Artigo> artigos;
+    private double valorTotal;
+}
